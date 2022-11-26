4cfbefc0c4f79cd4898223b085ed2d6226c94d41
dennis mwaniki <dennomaz@gmail.com>
2022-11-26 15:48:08 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase()
+}
+
+var bestCustomer = "bob"
+
+function setBestCustomer(){
+   var bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "Dennis"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Dennis M"
+}
